e75b718d14e82caa5f9cf13f46b0256ee9f1d10b
lalanuchie <76473023+NuchPunnawichP@users.noreply.github.com>
2024-11-03 17:35:37 +0700

    update

--- Assignment2/2_1_triangle.py
+++ Assignment2/2_1_triangle.py
@@ -5,8 +5,8 @@ def print_triangle(n):
         if i == n//2 + 2:
             print("* Homework 2  *", end = "")
         else:
-            for j in range(2 * i + 1):
-                if (i == n - 1) or (j == 0) or (j == 2 * i):
+            for j in range(2*i + 1):
+                if (i == n - 1) or (j == 0) or (j == 2*i):
                     print("*", end = "")
                 else:
                     print(" ", end = "")
