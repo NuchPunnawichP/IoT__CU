527783fec2f6f7af5a6c7d5af066adc064053214
lalanuchie <76473023+NuchPunnawichP@users.noreply.github.com>
2024-11-11 23:12:19 +0700

    update

--- Assignment3/3_3_LED.py
+++ Assignment3/3_3_LED.py
@@ -1,32 +1,36 @@
-import paho.mqtt.publish as publish # pip3 install paho-mqtt
+import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 import time
 import json
 import random
 import ssl
 
-port = 1883 # default port
+port = 1883
 Server_ip = "broker.netpie.io" 
 
-Alias = "Pi1"
+Subscribe_Topic = "@msg/LED"
+Publish_Topic = "@shadow/data/update"
 
-Subscribe_Topic = "@msg/Pi2/DHT11"
-Publish_Topic = "@msg/Pi1/DHT11"
-
-Client_ID = ""
-Token = ""
-Secret = ""
+Client_ID = "063c3874-0b64-45dc-9ff5-678e12dcebd8"
+Token = "kKP8kgMMBCRZ6ow2kkF7Ssv7pufUexR7"
+Secret = "VQPN6QRH5vi8EQPkXWF2mciWJHXWGfGs"
 
 MqttUser_Pass = {"username":Token,"password":Secret}
 
-# The callback for when the client receives a CONNACK response from the server.
+LED_Status = "on"
+sensor_data = {"Temp": 0, "Humi": 0, "LED" : LED_Status}
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe(Subscribe_Topic)
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    global LED_Status
+    data_receive = msg.payload.decode("UTF-8")
+    if data_receive == "on" or data_receive == "off":
+        LED_Status = data_receive  # Update LED status
+        print(f"LED Status updated to {LED_Status}")
+
 
 client = mqtt.Client(protocol=mqtt.MQTTv311,client_id=Client_ID, clean_session=True)
 client.on_connect = on_connect
@@ -38,12 +42,11 @@ client.connect(Server_ip, port)
 client.loop_start()
 
 while True:
-        data = {
-        "Alias" : Alias,
-        "Temp": random.randrange(30, 40),
-        "Humi": random.randrange(50, 80)
-        }
-        data_out=json.dumps(data) # encode object to JSON
+        sensor_data["Temp"] = random.randrange(30, 40)
+        sensor_data["Humi"] = random.randrange(50, 80)
+        sensor_data["LED"] = LED_Status
+        data_out=json.dumps({"data": sensor_data}) # encode object to JSON
+        print(data_out)
         client.publish(Publish_Topic, data_out, retain= True)
         print ("Publish.....")
         time.sleep(2)
